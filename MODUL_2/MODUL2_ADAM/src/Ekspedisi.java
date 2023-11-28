4d946d92c658cc7a87cc32925ce77d8d4ab86993
adaampuut <adam35046@gmail.com>
2023-11-28 15:41:30 +0700

    modul 2

---
+++ MODUL_2/MODUL2_ADAM/src/Ekspedisi.java
@@ -0,0 +1,19 @@
+public class Ekspedisi{
+    
+    int jumlahCabang;
+    
+    String pusat;
+
+    float tarif;
+
+    public Ekspedisi(int jumlahCabang, String pusat, float tarif) {
+        this.jumlahCabang = jumlahCabang;
+        this.pusat = pusat;
+        this.tarif = tarif;
+    }
+
+    public void informasi(){
+        System.out.println();
+    }
+
+}
---
+++ MODUL_2/MODUL2_ADAM/src/MainApp.java
@@ -0,0 +1,21 @@
+public class MainApp {
+    public static void main(String[] args){
+        
+        // TO DO: Create expedisi Object from Ekspedisi Class
+        System.out.println();
+        Ekspedisi Ekspedisisatu = new Ekspedisi(0, null, 0)
+        System.out.println("");
+
+        // TO DO: Create teluexpress Object from TelUExpress Class
+        System.out.println();
+        TelUExpress TelUExpresssatu = new TelUExpress(0, null, 0, false)
+        TelUExpresssatu = jumlahCabang
+        TelUExpresssatu = pusat
+        TelUExpresssatu = tarif
+        TelUExpresssatu = antarInter
+
+        // TO DO: Create friexpress Object from FRIExpress Class
+
+
+    }
+}
